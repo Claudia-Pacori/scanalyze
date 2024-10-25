c5b506c20bdd12d2cb3d9f307622140c4409155b
Claudia-Pacori <claudia.pacori@utec.edu.pe>
2024-10-25 15:33:06 -0400

    template customized 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -183,3 +183,31 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+
+
+// Get the modal
+var modal = document.getElementById("ctaModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("ctaButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
